c1a6130fbe3910e886ce4b1a41b9a8b47409bff5
zzaakiirr <zzaakiirr@gmail.com>
2018-03-04 18:36:56 +0300

    created movies recommendation system

--- main.py
+++ main.py
@@ -2,27 +2,77 @@ import urllib.request
 import urllib.parse
 import json
 
-
-def load_json_data_from_url(base_url, url_params):
-    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
-    response = urllib.request.urlopen(url).read().decode('utf-8')
-    return json.loads(response)
-
-
-def make_tmdb_api_request(method, api_key, extra_params=None):
-    extra_params = extra_params or {}
-    url = 'https://api.themoviedb.org/3%s' % method
-    params = {
-        'api_key': api_key,
-    }
-    params.update(extra_params)
-    return load_json_data_from_url(url, params)
-
 with open('movies_database.json') as f_obj:
     movies_database = json.load(f_obj)
 
-user_search_subtitle = input()
+
+def print_movies_from_list(movies_list):
+    for movie in movies_list:
+        print('\t' + movie)
+
+
+def is_in_database(entered_movie_title):
+    for movie in movies_database:
+        if movie['title'].lower() == entered_movie_title.lower():
+            return 1
+        else:
+            return 0
+
+
+def get_movie_info_from_database(entered_movie_title):
+    for movie in movies_database:
+        if movie['title'].lower() == entered_movie_title.lower():
+            return movie
+
+
+def get_attr(entered_movie_title, searching_attr):
+    for movie in movies_database:
+        if movie[searching_attr] == entered_movie_title[searching_attr]:
+            return movie[searching_attr]
+
+
+entered_movie_title = input()
+
+similar_title_movies = []
+similar_genre_movies = []
+similar_budget_movies = []
+equal_release_year_movies = []
+
+if  is_in_database(entered_movie_title):
+    entered_movie_info = get_movie_info_from_database(entered_movie_title)
+
+    entered_movie_genres = get_attr(entered_movie_info, 'genres')
+    entered_movie_budget = int(get_attr(entered_movie_info, 'budget'))
+    entered_movie_release_year = int(get_attr(entered_movie_info,
+        'release_date')[:4])
 
 for movie in movies_database:
-    if movie['original_title'].lower().find(user_search_subtitle.lower()) != -1:
-        print(movie['original_title'])
+    if (movie['title'].lower().find(entered_movie_title.lower()) != -1):
+        similar_title_movies.append(movie['title'])
+
+    if is_in_database(entered_movie_title):
+        if (entered_movie_budget + 10000 > movie['budget'] or
+                entered_movie_budget - 10000 < movie['budget']):
+            similar_budget_movies.append(movie['title'])
+
+        if entered_movie_release_year == int(movie['release_date'][:4]):
+            equal_release_year_movies.append(movie['title'])
+
+        for entered_movie_genre in entered_movie_genres:
+            for current_movie_genre in movie['genres']:
+                if entered_movie_genre == current_movie_genre:
+                    similar_genre_movies.append(movie['title'])
+
+
+print('\nMovies with similar title:')
+print_movies_from_list(similar_title_movies)
+
+if is_in_database(entered_movie_title):
+    print('\nMovies with similar budget:')
+    print_movies_from_list(similar_budget_movies)
+
+    print('\nMovies with the same release year:')
+    print_movies_from_list(equal_release_year_movies)
+
+    print('\nMovies with the same genres:')
+    print_movies_from_list(similar_genre_movies)
