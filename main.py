147d6ce137da4e643ee462cb20d85ba2228cc3b0
zzaakiirr <zzaakiirr@gmail.com>
2018-03-04 13:33:04 +0300

    added functions for making reauests to 'tmdb' & load this data

---
+++ main.py
@@ -0,0 +1,20 @@
+import urllib.request
+import urllib.parse
+import json
+
+
+def load_json_data_from_url(base_url, url_params):
+    url = '%s?%s' % (base_url, urllib.parse.urlencode(url_params))
+    response = urllib.request.urlopen(url).read().decode('utf-8')
+    return json.loads(response)
+
+
+def make_tmdb_api_request(method, api_key, extra_params=None):
+    extra_params = extra_params or {}
+    url = 'https://api.themoviedb.org/3%s' % method
+    params = {
+        'api_key': api_key,
+        'language': 'ru',
+    }
+    params.update(extra_params)
+    return load_json_data_from_url(url, params)
