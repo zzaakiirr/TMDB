096789310b49436e9bdf1b8ba3514196a32c3be5
zzaakiirr <zzaakiirr@gmail.com>
2018-03-12 15:17:15 +0300

    Added doing program only while it runs by self
    Used __name__ == '__main__'

--- has_subtitle_movies.py
+++ has_subtitle_movies.py
@@ -1,10 +1,11 @@
 import json
 
-with open('movies_database.json') as f_obj:
-    movies_database = json.load(f_obj)
+if __name__ == '__main__':
+    with open('movies_database.json') as f_obj:
+        movies_database = json.load(f_obj)
 
-user_search_subtitle = input()
+    user_search_subtitle = input()
 
-for movie in movies_database:
-    if user_search_subtitle.lower() in movie['title'].lower():
-        print(movie['title'])
+    for movie in movies_database:
+        if user_search_subtitle.lower() in movie['title'].lower():
+            print(movie['title'])
