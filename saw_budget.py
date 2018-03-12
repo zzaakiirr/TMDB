aa9439ac43d9ba7c2c7512f5295b70fc42da5951
zzaakiirr <zzaakiirr@gmail.com>
2018-03-12 15:13:42 +0300

    Fixed printing only while programm runs by self

--- saw_budget.py
+++ saw_budget.py
@@ -2,6 +2,7 @@ import os
 import helpers
 api_key = os.environ.get('TMDB_key')
 
-print(helpers.make_tmdb_api_request(
-    '/movie/215',
-    api_key=api_key)['budget'])
+if __name__ == '__main__':
+    print(helpers.make_tmdb_api_request(
+        '/movie/215',
+        api_key=api_key)['budget'])
