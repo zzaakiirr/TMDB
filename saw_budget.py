a53f86e1866e307fd613f451179e60cb36056574
zzaakiirr <zzaakiirr@gmail.com>
2018-03-14 22:10:39 +0300

    Fixed getting api_key only while running programm by self
    Used if __name__ == '__main__' to fix

--- saw_budget.py
+++ saw_budget.py
@@ -1,8 +1,9 @@
 import os
 import helpers
-api_key = os.environ.get('TMDB_key')
+
 
 if __name__ == '__main__':
+    api_key = os.environ.get('TMDB_key')
     print(helpers.make_tmdb_api_request(
         '/movie/215',
         api_key=api_key)['budget'])
